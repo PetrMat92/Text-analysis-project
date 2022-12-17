0d33a1b3f269c23d83b71c2faed3b714aac99df7
Petr Matějíček <114687356+PetrMat92@users.noreply.github.com>
2022-12-17 01:18:05 +0100

    Update Bulls&cows.py

--- Bulls&cows.py
+++ Bulls&cows.py
@@ -5,26 +5,54 @@ email: matejicekpetr92@gmail.com
 discord: Petr_Mat#1921
 """
 # imports
-
 import random
 from timeit import default_timer as timer
 import datetime as dt
+from pyfiglet import Figlet
 
 # global variables
 
-separator = 35 * "──"
-
+separator = 36 * "──"
 # functions
+
+
+def welcome_message() -> None:
+    """
+        welcome_message function performs following steps:
+        1. prints "Bulls and Cows" header
+        2. prints instructions
+        :return: None
+        """
+    f = Figlet(font='slant')
+    print(f.renderText('Bulls and Cows '))
+    print(f'Instructions: '
+          f'\n{separator}'
+          f'\n 1. Your task is to guess a secret number '
+          f'\n 2. The number to be guessed is a 4 digit number'
+          f'\n 3. Secret number comprises of unique digits ranging from 0 - 9'
+          f'\n 4. First digit cannot start with 0'
+          f'\n 5. Bulls indicate the number of correct digits in the correct position'
+          f'\n 6. Cows indicates the number of correct digits in the wrong position.'
+          f'\n{separator}')
 
 
 def random_number_generator() -> str:
     """
     random_number_generator function performs following steps:
-    1. generates unique 4-digit code in the range of 1 to 9
-    2. stores number in "code" variable
+    1. initiates while cyclus to generate unique 4-digit number in the range of 0 to 9
+    2. initiates while cyclus to ensure that 0 is not on the first index of number
+    3. converts variable number to string and stores the value in variable code
     :return: code
     """
-    code = ''.join(map(str, random.sample(range(1, 9), 4)))
+    number = []
+    while len(number) < 4:
+        x = random.randrange(0, 9)  # generates unique random numbers and appends them to "number" list
+        if x not in number:
+            number.append(x)
+    while number[0] == 0:  # shuffles the code until a zero is not on the first digit position
+        random.shuffle(number)
+
+    code = ''.join(str(x) for x in number)
     return code
 
 
@@ -40,10 +68,20 @@ def game_round() -> str:
     """
     while True:
         guess = input(f'{separator} \nEnter a number: ')
-        if len(guess) == 4 and guess.isnumeric():
+        if len(guess) == 4 and guess.isnumeric() and guess[0] != "0" and len(set(guess)) == 4:
             return guess
+        elif guess[0] == "0":
+            print(f'{separator} '
+                  f'\n---> {guess}'
+                  f'\nSecret number cannot start with 0, Try again!!!')
+        elif len(set(guess)) < 4:
+            print(f'{separator} '
+                  f'\n---> {guess}'
+                  f'\nYour guess must contain 4 unique digits. Try again!!!')
         else:
-            print(f'{separator} \n---> {guess} \nYou must insert 4 unique digits from 1 to 9. Try again!')
+            print(f'{separator} '
+                  f'\n---> {guess} '
+                  f'\nYou must insert 4 unique digits. Try again!!!')
 
 
 def game_analysis(guess, code) -> tuple:
@@ -70,7 +108,8 @@ def game_analysis(guess, code) -> tuple:
 def main() -> None:
     """
        main function performs following steps:
-       1. loads the "code" value from the appropriate function
+    0. ---> calls welcome_message function that prints logo and instructions
+        1. loads the "code" value from the appropriate function
           1.1 prints a welcome message
           1.2 presents the rules
           1.3 starts the countdown
@@ -86,9 +125,10 @@ def main() -> None:
           2.5 if guess is not equal to code ---> function prints numbers of bulls and cows
        :return: None
        """
+    welcome_message()
     code = random_number_generator()
+    print(f"Lets start with your first guess!!!")
     print(f"For testing. Code is: {code}")  # prints code for testing
-    print(f"{separator}\nGuess my number. It contains 4 unique digits from 1 to 9")
     guess_list = []  # list of users guesses
     start = timer()  # starts counting time
     while True:
@@ -101,18 +141,28 @@ def main() -> None:
             print(f"{separator} \n---> {guess} \nYour have won! ||"
                   f" Time: {dt.timedelta(seconds=end - start)} (hh:mm:ss.ms) ||"
                   f" Attempts: {len(guess_list)}")
-            print(f'{separator} \nYou can find history of your valid attempts below: \n{"-" * 70}')
+            print(f'{separator} '
+                  f'\nYou can find the history of your valid attempts below: '
+                  f'\n{"-" * 70}')
             print("\n".join([" ".join(guess_list[i:i + 14]) for i in range(0, len(guess_list), 5)]))
             break
         if guess != code:
             if bull == 1 and cow == 1:
-                print(f'{separator} \n---> {guess} \n{bull} bull, {cow} cow')
+                print(f'{separator} \n'
+                      f'---> {guess} \n'
+                      f'{bull} bull, {cow} cow')
             elif bull == 1 and cow != 1:
-                print(f'{separator} \n---> {guess} \n{bull} bull, {cow} cows')
+                print(f'{separator} \n'
+                      f'---> {guess} \n'
+                      f'{bull} bull, {cow} cows')
             elif bull != 1 and cow == 1:
-                print(f'{separator} \n---> {guess} \n{bull} bulls, {cow} cow')
+                print(f'{separator} '
+                      f'\n---> {guess} '
+                      f'\n{bull} bulls, {cow} cow')
             else:
-                print(f'{separator} \n---> {guess} \n{bull} bulls, {cow} cows')
+                print(f'{separator} '
+                      f'\n---> {guess} '
+                      f'\n{bull} bulls, {cow} cows')
 
 
 if __name__ == '__main__':
